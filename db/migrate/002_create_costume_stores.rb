a5ca20c8d0252b7595948cb212f67321b9d597df
Joy Lin <vvlnote@hotmail.com>
2019-04-22 23:07:53 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,7 +8,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      t.boolean
+      t.boolean :
     end
   end
   
