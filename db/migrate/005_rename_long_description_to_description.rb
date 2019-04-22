4fe418066d8e14ba5aec707fc98d59f286dffea4
Joy Lin <vvlnote@hotmail.com>
2019-04-22 23:46:00 +0000

    Automatically backed up by Learn

--- db/migrate/005_rename_long_description_to_description.rb
+++ db/migrate/005_rename_long_description_to_description.rb
@@ -1,6 +1,6 @@
 class RenameLongDescriptionToDescription < ActiveRecord::Migration[5.1]
   
   def change
-    rename_column :haunted_house, :long_description, :description
+    rename_column :haunted_houses, :long_description, :description
   end
 end
