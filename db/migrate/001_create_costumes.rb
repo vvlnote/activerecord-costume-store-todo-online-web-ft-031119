8297d838ad917f0adb4c3d916fcc2af139297fcc
Joy Lin <vvlnote@hotmail.com>
2019-04-22 22:53:00 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
     create_table :costumes do |t|
       t.string :name
       t.real :price
-      t.integer :size
+      t.string :size
       t.text :image_url
       t.timestamp :created_at
       t.timestamp :updated_at
