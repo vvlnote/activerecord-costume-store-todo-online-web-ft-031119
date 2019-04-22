20b79fcafbcb09217b7be12007abc0abc35ca2f5
Joy Lin <vvlnote@hotmail.com>
2019-04-22 23:14:02 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+  
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.theme
+    end
+    
+    
+  end
+  
+end
