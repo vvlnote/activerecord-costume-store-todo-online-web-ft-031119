c7772a9d88ff63e4511650ead5d9bd35f67392d6
Joy Lin <vvlnote@hotmail.com>
2019-04-22 23:31:28 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.boolean "family_friendly"
     t.date "opening_date"
     t.date "closing_date"
+    t.text "long_description"
   end
 
 end
