2e1216fafb56946ca4fd6cff51ef46eef61d24ce
Joy Lin <vvlnote@hotmail.com>
2019-04-22 23:11:06 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,7 +9,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.datetime
+      t.timestamp :opening_time
+      t.timestamp :
     end
   end
   
